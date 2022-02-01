be81b53737ab7e67a0af4eb6d24da9a44025c569
yukihane <yukihane.feather@gmail.com>
2022-02-02 04:25:37 +0900

    問題再現コード

---
+++ src/main/java/example/JavaMain.java
@@ -0,0 +1,8 @@
+package example;
+
+public class JavaMain {
+    public static void main(String[] args) {
+        MyValue v = MyValue.of(10);
+        System.out.println(v.getValue());
+    }
+}
--- src/main/kotlin/example/Main.kt
+++ src/main/kotlin/example/Main.kt
@@ -3,4 +3,7 @@ package example
 fun main() {
     val obj = MyJavaClass()
     println(obj.message)
+
+    val v = MyValue.of(10)
+    println(v.value.toString())
 }
